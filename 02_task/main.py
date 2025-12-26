41e0e9f12ac337b3780b5cbd853c8e33ef0fca5d
Vasyl Domanchuk <domanchukits@gmail.com>
2025-12-26 18:40:04 +0100

    implement task 2

---
+++ 02_task/main.py
@@ -0,0 +1,17 @@
+from node import insert, min_tree_value
+
+
+def main():
+    # prefill tree
+    root = None
+    keys = [10, 20, 30, 25, -4, 28, 27, -1]
+
+    for key in keys:
+        root = insert(root, key)
+
+    max_value = min_tree_value(root)
+    print(f"Min value is: {max_value}")  # -4
+
+
+if __name__ == '__main__':
+    main()
